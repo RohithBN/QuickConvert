132c1ef2702be9db092d8ce795043d31339354bb
RohithBN <rohithbn27@gmail.com>
2025-12-13 20:07:41 +0530

    resize using NN interpol

--- backend/go/lib/lib.go
+++ backend/go/lib/lib.go
@@ -1,13 +1,26 @@
 package lib
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 type handlerFunc func(c *gin.Context) error
 
 func GinErrorWrapper(handlerFunc handlerFunc) func(c *gin.Context) {
-	return func (c *gin.Context)  {
-		if err:= handlerFunc(c); err != nil {
-		c.JSON(500, gin.H{"error": err.Error(), "message": "Internal Server Error"})
+	return func(c *gin.Context) {
+		if err := handlerFunc(c); err != nil {
+			c.JSON(500, gin.H{"error": err.Error(), "message": "Internal Server Error"})
+		}
 	}
+}
+
+func ParseStringToInt(str string) (int, error) {
+	var result int
+	_, err := fmt.Sscanf(str, "%d", &result)
+	if err != nil {
+		return 0, err
 	}
+	return result, nil
 }
